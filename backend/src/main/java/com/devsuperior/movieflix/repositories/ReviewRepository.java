120978606a81d3675f1e39933bddfeda8ac2dc04
Júnior de Oliveira <junior_siq90@hotmail.com>
2021-05-25 20:40:31 -0300

    GenreDTO, GenreResource e GnreService

--- backend/src/main/java/com/devsuperior/movieflix/repositories/ReviewRepository.java
+++ backend/src/main/java/com/devsuperior/movieflix/repositories/ReviewRepository.java
@@ -3,8 +3,7 @@ package com.devsuperior.movieflix.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.devsuperior.movieflix.entities.Review;
-import com.devsuperior.movieflix.entities.pk.ReviewPK;
 
-public interface ReviewRepository extends JpaRepository<Review, ReviewPK>{
+public interface ReviewRepository extends JpaRepository<Review, Long>{
 
 }
